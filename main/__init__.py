fbba7b3cd996d9751d7f7568039fdc19f3c25062
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2023-12-19 22:54:27 -0300

    deploy

--- main/__init__.py
+++ main/__init__.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 import pymysql
 from flask_wtf.csrf import CSRFProtect
 
-
 pymysql.install_as_MySQLdb()
 db = SQLAlchemy()
 csrf = CSRFProtect()
@@ -16,19 +15,28 @@ jwt = JWTManager()
 login_manager = LoginManager()
 
 
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        # Implementa la lógica para manejar otros tipos de objetos si es necesario
+        return super().default(obj)
+
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'aaaaaaaaaaaaa121321315321'
     load_dotenv()
     login_manager.init_app(app)
+
     # Configuracion de la conexion a la base de datos
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + os.getenv('DATABASE_USER') + ':' + os.getenv(
         'DATABASE_PASSWORD') \
                                             + '@' + os.getenv('DATABASE_URL') + ':' + os.getenv('DATABASE_PORT') \
                                             + '/' + os.getenv('DATABASE_NAME')
+
     csrf.init_app(app)
     db.init_app(app)
+
     from main.resources import user, home, cars
     app.register_blueprint(home, url_prefix='/')
     app.register_blueprint(user, url_prefix='/')
@@ -36,7 +44,9 @@ def create_app():
 
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
-    app.json_encoder = JSONEncoder
+
+    app.json_encoder = CustomJSONEncoder  # Usa la clase personalizada
+
     jwt.init_app(app)
 
     return app
