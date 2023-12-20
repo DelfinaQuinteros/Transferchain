9fba91abec8f1870f0be5f2edd718ff18b1ed051
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2023-12-19 22:58:46 -0300

    deploy

--- main/__init__.py
+++ main/__init__.py
@@ -3,7 +3,6 @@ from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from flask.json import JSONEncoder
 from dotenv import load_dotenv
 import pymysql
 from flask_wtf.csrf import CSRFProtect
@@ -15,28 +14,19 @@ jwt = JWTManager()
 login_manager = LoginManager()
 
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        # Implementa la lógica para manejar otros tipos de objetos si es necesario
-        return super().default(obj)
-
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'aaaaaaaaaaaaa121321315321'
     load_dotenv()
     login_manager.init_app(app)
-
     # Configuracion de la conexion a la base de datos
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + os.getenv('DATABASE_USER') + ':' + os.getenv(
         'DATABASE_PASSWORD') \
                                             + '@' + os.getenv('DATABASE_URL') + ':' + os.getenv('DATABASE_PORT') \
                                             + '/' + os.getenv('DATABASE_NAME')
-
     csrf.init_app(app)
     db.init_app(app)
-
     from main.resources import user, home, cars
     app.register_blueprint(home, url_prefix='/')
     app.register_blueprint(user, url_prefix='/')
@@ -44,9 +34,6 @@ def create_app():
 
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
-
-    app.json_encoder = CustomJSONEncoder  # Usa la clase personalizada
-
     jwt.init_app(app)
 
     return app
